17f58d9fea00dcc7a604ca6083b1a70091df4918
Kevin Molid <kevinmolid@gmail.com>
2024-03-18 22:08:19 +0100

    set up add exercice modal as component

--- src/pages/Exercises.jsx
+++ src/pages/Exercises.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import exercises from '../data/exercises.jsx'
 import ExerciseCard from '../components/ExerciseCard.jsx'
+import AddExerciseModal from '../components/AddExerciseModal.jsx'
 
 const exercisesHTML = exercises.map(exercise => {
 
@@ -10,110 +11,25 @@ const exercisesHTML = exercises.map(exercise => {
 })
 
 function Exercises() {
-    return (
-      <main>
-        <div className='exercises--heading-wrapper'>
-          <h2>Exercises</h2>
-          <button className='btn btn-primary btn-small'><i class="fa-solid fa-plus"></i></button>
-        </div>
-        <div className='new-exercise-modal'>
-          <h3>Add exercise</h3>
-            <form action="">
-              <label htmlFor="exercise-name">Exercise name</label>
-              <input type="text" id="exercise-name"/>
-              <label htmlFor="exercise-category">Category</label>
-              <select name="category" id="exercise-category">
-                <option value="standard">Strength</option>
-                <option value="standard">Calisthenics</option>
-                <option value="standard">Cardio</option>
-              </select>
-              <details>
-                <summary>Primary muscles</summary>
-                <form>
-                  <fieldset>
-                    <legend>
-                      Cars
-                    </legend>
-                    <ul>
-                      <li>
-                        <label for="primary-chest">Chest</label>
-                        <input type="checkbox" id="primary-chest" name="chest" value="chest" />
-                      </li>
-                      <li>
-                        <label for="primary-shoulders">Shoulders</label>
-                        <input type="checkbox" id="primary-shoulders" name="shoulders" value="shoulders" />
-                      </li>
-                      <li>
-                        <label for="primary-legs">Legs</label>
-                        <input type="checkbox" id="primary-legs" name="legs" value="legs" />
-                      </li>
-                      <li>
-                        <label for="primary-back">Back</label>
-                        <input type="checkbox" id="primary-back" name="back" value="back" />
-                      </li>
-                      <li>
-                        <label for="primary-traps">Traps</label>
-                        <input type="checkbox" id="primary-traps" name="traps" value="traps" />
-                      </li>
-                      <li>
-                        <label for="primary-biceps">Biceps</label>
-                        <input type="checkbox" id="primary-biceps" name="biceps" value="biceps" />
-                      </li>
-                      <li>
-                        <label for="primary-triceps">Triceps</label>
-                        <input type="checkbox" id="primary-triceps" name="triceps" value="triceps" />
-                      </li>
-                    </ul>
-                  </fieldset>
-                </form>
-              </details>
+  const [modalExpanded, setModalExpanded] = React.useState(false)
 
-              <details>
-                <summary>Secondary muscles</summary>
-                <form>
-                  <fieldset>
-                    <legend>
-                      Cars
-                    </legend>
-                    <ul>
-                      <li>
-                        <label for="secondary-chest">Chest</label>
-                        <input type="checkbox" id="secondary-chest" name="chest" value="chest" />
-                      </li>
-                      <li>
-                        <label for="secondary-shoulders">Shoulders</label>
-                        <input type="checkbox" id="secondary-shoulders" name="shoulders" value="shoulders" />
-                      </li>
-                      <li>
-                        <label for="secondary-legs">Legs</label>
-                        <input type="checkbox" id="secondary-legs" name="legs" value="legs" />
-                      </li>
-                      <li>
-                        <label for="secondary-back">Back</label>
-                        <input type="checkbox" id="secondary-back" name="back" value="back" />
-                      </li>
-                      <li>
-                        <label for="secondary-traps">Traps</label>
-                        <input type="checkbox" id="secondary-traps" name="traps" value="traps" />
-                      </li>
-                      <li>
-                        <label for="secondary-biceps">Biceps</label>
-                        <input type="checkbox" id="secondary-biceps" name="biceps" value="biceps" />
-                      </li>
-                      <li>
-                        <label for="secondary-triceps">Triceps</label>
-                        <input type="checkbox" id="secondary-triceps" name="triceps" value="triceps" />
-                      </li>
-                    </ul>
-                  </fieldset>
-                </form>
-              </details>
-              <button className='btn btn-primary'>Add exercise</button>
-            </form>
-          </div>
-        {exercisesHTML}
-      </main>
-    )
+  function toggleModal() {
+    setModalExpanded(prevState => !prevState)
   }
-  
-  export default Exercises
+
+  return (
+    <main>
+      <div className='exercises--heading-wrapper'>
+        <h2>Exercises</h2>
+        <button onClick={toggleModal}
+          className='btn btn-primary btn-small'>
+          {modalExpanded && <i class="fa-solid fa-x"></i>}
+          {!modalExpanded && <i class="fa-solid fa-plus"></i>}</button>
+      </div>
+      {modalExpanded && <AddExerciseModal />}
+      {exercisesHTML}
+    </main>
+  )
+}
+
+export default Exercises
