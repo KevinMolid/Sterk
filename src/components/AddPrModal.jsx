30ecc6589a1b243e094c4e1c4850bc4266cf7388
Kevin Molid <kevinmolid@gmail.com>
2024-03-27 14:52:13 +0100

    Styled inputs

--- src/components/AddPrModal.jsx
+++ src/components/AddPrModal.jsx
@@ -11,17 +11,19 @@ function AddPrModal() {
             <button className='btn-close' onClick={togglePrModal}>
                 <i className="fa-solid fa-x"></i>
             </button>
-            <h3>Add PR</h3>
+            <h3 className='margin-bottom-1'>Add PR</h3>
             <form action="">
-                <select name="select-pr" id="select-pr">
-                    <option value="">-- Choose exercise --</option>
-                    <option value="Squat">Squat</option>
-                    <option value="Bench press">Bench press</option>
-                    <option value="Deadlift">Deadlift</option>
-                    <option value=""></option>
-                    <option value=""></option>
-                </select>
-                <input type="number" />
+                <div className='grid-2-col margin-bottom-1'>
+                    <select name="select-pr" id="select-pr">
+                        <option value="">-- Choose exercise --</option>
+                        <option value="Squat">Squat</option>
+                        <option value="Bench press">Bench press</option>
+                        <option value="Deadlift">Deadlift</option>
+                        <option value=""></option>
+                        <option value=""></option>
+                    </select>
+                    <input type="number" />
+                </div>
                 <button className='btn btn-primary'>Add PR</button>
             </form>
         </div>
