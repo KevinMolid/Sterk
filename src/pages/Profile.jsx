afae4ce79e31f276ba1165aa1f9dadc7a0595e32
Kevin Molid <kevinmolid@gmail.com>
2024-03-28 12:22:22 +0100

    Fetching and rendering PRs in profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,43 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import AddPrModal from '../components/AddPrModal'
+
+import { doc, getDoc } from 'firebase/firestore'
+import { db } from '../config.jsx'
+
 
 function Profile({ user, exercises }) {
   const [prModalExpanded, setPrModalExpanded] = useState(false)
+  const [prs, setPrs] = useState([])
+
+  useEffect(() => {
+    const getPrs = async () => {
+      const userPrs = await fetchUserPRs(user.uid);
+      setPrs(userPrs);
+    };
+
+    if (user.uid) {
+      getPrs();
+    }
+  }, [user]);
+
+  async function fetchUserPRs(userId) {
+    // Reference to the user's document in the 'users' collection
+    const userRef = doc(db, 'users', userId);
+  
+    try {
+      // Attempt to fetch the document
+      const docSnap = await getDoc(userRef);
+  
+      if (docSnap.exists()) {
+        return docSnap.data().PRs; // Return the PRs field
+      } else {
+        return []; // Consider returning an empty array or null based on your app's needs
+      }
+    } catch (error) {
+      console.error("Error fetching user PRs:", error);
+      return []; // Consider how you want to handle errors, possibly returning an empty array or null
+    }
+  }
 
   function togglePrModal() {
     setPrModalExpanded(prevState => !prevState)
@@ -40,6 +75,11 @@ function Profile({ user, exercises }) {
           </div>
           {prModalExpanded && <AddPrModal user={user} exercises={exercises}/>}
           <p className="flex-space"><span>Bench press</span>77.5 kg</p>
+          {prs.map((pr, index) => (
+            <div key={index}>
+              <p className="flex-space"><span>{pr.name}</span> {pr.value} kg</p>
+            </div>
+          ))}
         </div>
       </main>
     )
