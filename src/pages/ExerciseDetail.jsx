bee86e1b664403b0dec52cf5313288acaca2ba44
Kevin Molid <kevinmolid@gmail.com>
2024-04-06 22:07:37 +0200

    Cleaned up code

--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -1,7 +1,7 @@
 import { React, useState, useEffect } from 'react'
 import { useParams } from "react-router-dom"
-import { doc, getDoc } from "firebase/firestore";
-import { db } from '../config';
+import { doc, getDoc } from "firebase/firestore"
+import { db } from '../config'
 
 export default function ExerciseDetail(){
     const [exercise, setExercise] = useState('')
@@ -13,23 +13,23 @@ export default function ExerciseDetail(){
                 setExercise(exercise)
             })
             .catch((error) => {
-                // Handle any errors that occurred during fetch
-            });
-    }, []); // Empty dependency array means this effect runs once on mount
+                throw error
+            })
+    }, [exerciseId])
 
     async function fetchExerciseById(exerciseId) {
-        const exerciseRef = doc(db, "exercises", exerciseId);
+        const exerciseRef = doc(db, "exercises", exerciseId)
         try {
-            const docSnap = await getDoc(exerciseRef);
+            const docSnap = await getDoc(exerciseRef)
             if (docSnap.exists()) {
-                return docSnap.data(); // Returns the exercise data if found
+                return docSnap.data() // Returns the exercise data if found
             } else {
-                console.log("No such exercise!");
-                return null; // Handle the case where the exercise does not exist
+                console.log("No such exercise!")
+                return null // Handle the case where the exercise does not exist
             }
         } catch (error) {
-            console.error("Error fetching exercise:", error);
-            throw error; // Optionally re-throw the error for further handling
+            console.error("Error fetching exercise:", error)
+            throw error // Optionally re-throw the error for further handling
         }
     }
 
