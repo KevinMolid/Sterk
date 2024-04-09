021a58cc45c588eecbbc5ffafb1b9409f59635c4
Kevin Molid <kevinmolid@gmail.com>
2024-04-09 15:39:52 +0200

    a -> Link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import sterkLogo from '/src/assets/logoBlack.png'
+import { Link } from 'react-router-dom'
 
 function Header(props) {
     return (
         <header className='header'>
-            <a href='/'>
+            <Link to='/'>
                 <div className='header--logo-wrapper'>
                     <img className='header--logo-img' src={sterkLogo} alt="Sterk logo" />
                     <span className='header--logo-txt'>Sterk</span>
                 </div>
-            </a>
+            </Link>
             <div className='header--btns'>
-                <a href='/findfriends'>
+                <Link to='/findfriends'>
                     <button className='btn btn-tertiary btn-small'><i className="fa-solid fa-magnifying-glass"></i></button>
-                </a>
+                </Link>
                 <button className='btn btn-tertiary btn-small' onClick={props.signOut}><i className="fa-solid fa-arrow-right-from-bracket"></i></button>
             </div>
         </header>
