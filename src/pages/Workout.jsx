bd3986d6ec353bf59b3fa0d84532dd706c4df779
Kevin Molid <kevinmolid@gmail.com>
2024-04-27 16:49:17 +0200

    Added buttons to cancel or finish wo

--- src/pages/Workout.jsx
+++ src/pages/Workout.jsx
@@ -7,20 +7,26 @@ export default function Workout(){
 
     return (
         <main>
-            <div className='margin-bottom-1'>
+            <div className='margin-bottom-1 flex-space'>
                 <Link className="link" 
                     to={`/workouts`}>
                     <i className="fa-solid fa-arrow-left"></i> {`Back to workouts`}
                 </Link>
+                <button className='btn-txt'>Finish workout</button>
             </div>
             <h2 className='margin-bottom-1'>{activeWorkout.name}</h2>
             {activeWorkout.exercises && activeWorkout.exercises.map(exercise => {
                 return (
-                    <div key={exercise.name}>
+                    <div key={exercise.name} className='margin-bottom-1'>
                         <h3>{exercise.name}</h3>
+                        {exercise.sets.map((set, index) => {
+                            return(
+                                <p key={exercise.name + index}>{set.reps} x {set.weight}kg</p>
+                            )
+                        })}
                     </div>
             )})}
-            <button className='btn btn-primary'>Finish workout</button>
+            <button className='btn btn-txt'>Cancel workout</button>
         </main>
     )
 }
