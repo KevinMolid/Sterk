68aff780bf3892aef0117f8c21f7d7a858dc4616
Kevin Molid <kevinmolid@gmail.com>
2024-05-02 20:09:30 +0200

    Added placeholder functions

--- src/pages/Workout.jsx
+++ src/pages/Workout.jsx
@@ -1,9 +1,29 @@
-import { React, useContext } from 'react'
+import { React, useState, useContext, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import UserContext from '../UserContext.jsx'
 
 export default function Workout(){
     const { activeWorkout } = useContext(UserContext)
+    const [ exercises, setExercises ] = useState(activeWorkout.exercises)
+
+    useEffect(() => {
+        setExercises(activeWorkout.exercises)
+    },  [activeWorkout])
+
+    function handleInput(e) {
+        e.target.value = e.target.value
+    }
+
+    function finishWorkout() {
+        console.log('Workout completed!')
+    }
+
+    function cancelWorkout() {
+        console.log('Workout canceled!')
+    }
+
+
+
 
     return (
         <main>
@@ -12,10 +32,10 @@ export default function Workout(){
                     to={`/workouts`}>
                     <i className="fa-solid fa-arrow-left"></i> {`Back to workouts`}
                 </Link>
-                <button className='btn-txt'>Finish workout</button>
+                <button className='btn-txt' onClick={finishWorkout}>Finish workout</button>
             </div>
             <h2 className='margin-bottom-1'>{activeWorkout.name}</h2>
-            {activeWorkout.exercises && activeWorkout.exercises.map(exercise => {
+            {exercises && exercises.map(exercise => {
                 return (
                     <div key={exercise.name} className='margin-bottom-1'>
                         <h3 className='margin-bottom-half'>{exercise.name}</h3>
@@ -29,15 +49,15 @@ export default function Workout(){
                             return(
                                 <div className='grid-4-col margin-bottom-half' key={exercise.name + index}>
                                     <p>{index + 1}</p>
-                                    <input type="number" value={set.weight}/>
-                                    <input type="number" value={set.reps}/>
+                                    <input type="number" value={set.weight} onChange={handleInput}/>
+                                    <input type="number" value={set.reps} onChange={handleInput}/>
                                     <button className='btn-complete'><i className="fa-solid fa-check"></i></button>
                                 </div>
                             )
                         })}
                     </div>
             )})}
-            <button className='btn btn-txt'>Cancel workout</button>
+            <button className='btn btn-txt' onClick={cancelWorkout}>Cancel workout</button>
         </main>
     )
 }
